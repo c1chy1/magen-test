6e62d3244cf8049eab5fd9cdace1366b316bab7a
c1chy1 <36567964+c1chy1@users.noreply.github.com>
2023-10-13 13:18:55 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -7,36 +7,3 @@
   </main>
 
 </template>
-<script setup lang="ts">
-
-
-
-
-
-
-
-import {useCategories} from "~/composables/useCategories";
-
-const props = defineProps({
-  categoryId: { type: String, required: false },
-  categorySlug: { type: String, required: false },
-});
-
-
-
-
-
-/*const products = computed(() => {
-  return (
-
-      useCategories().state.products
-  );
-});
-
-console.log(products)*/
-
-
-
-
-
-</script>
